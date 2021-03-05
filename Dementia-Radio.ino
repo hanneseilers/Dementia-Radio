4b957b675517db47261dc5f88a845de681b128fa
Hannes Eilers <mail@hanneseilers.de>
2021-03-05 08:37:22 +0100

    Improved volume control. Optimized code by reducing Serial output.

--- Dementia-Radio.ino
+++ Dementia-Radio.ino
@@ -2,6 +2,7 @@
  * Playing files from SD Card (or web stream, if wanted)
  * Requires ESP32 libraries installed: https://github.com/espressif/arduino-esp32/releases/latest/
  */
+#include <Arduino.h>
 #include <WiFi.h>
 #include <Ticker.h>
 #include <SD.h>
@@ -30,8 +31,8 @@
 
 #define SWITCH_ON_TICK_MS       100
 
-#define ADC_RESOLUTION  12      // BIT
-#define ADC_MAX         4096.0  // 2 ^ADC_RESOLUTION (double!)
+#define ADC_RESOLUTION  9      // BIT
+#define ADC_MAX         511.0  // 2 ^ADC_RESOLUTION (double!)
 
 #define FILES_MAX                200     // max. uint8_t-1!
 #define FILES_ENDING            ".mp3"
@@ -46,11 +47,11 @@
 #define SPI_SCK       18
 
 #define I2S_DOUT      25
-#define I2S_BCLK      26
-#define I2S_LRC       27
+#define I2S_BCLK      26            // 27
+#define I2S_LRC       27            // 26
 
 #define ADC_VOLUME    35
-#define SWITCH_ON     12
+#define SWITCH_ON     12            // 14
 #define SWITCH_GPIO   GPIO_NUM_12
 
 #define IO_LED        1
@@ -63,9 +64,9 @@ Ticker tickerSwitch;
 uint8_t lastVolume;
 bool deviceOn;
 
-String filesList[200];
-uint8_t nFilesLeft;
-uint8_t nFilesPlayed;
+String filesList[200];    // list of available file names
+uint8_t nFilesLeft;       // count of files still to play
+uint8_t nFilesPlayed;     // count of files already played
 
 
 
@@ -85,7 +86,8 @@ void setup() {
   Serial.println(VERSION);
 
   // configure sleep & wakeup
-  esp_sleep_enable_ext0_wakeup( SWITCH_GPIO, 1);
+  esp_sleep_enable_ext0_wakeup( SWITCH_GPIO, 1 );
+  gpio_pulldown_en( SWITCH_GPIO );
   esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON); // all RTC Peripherals are powered
 
   // if wakeup was caused by switch on
@@ -237,15 +239,22 @@ void checkSwitch() {
  * Checks if volume level changed
  */
 void onVolume() {
-  double adcValue = analogRead(ADC_VOLUME);
+
+  long adcValue = analogRead(ADC_VOLUME);
+  
+  for( uint8_t i=0; i++; i < 50){
+    adcValue = adcValue + analogRead(ADC_VOLUME);
+    adcValue = adcValue / 2;
+  }
+  
   //Serial.println( adcValue );
   uint8_t volume = ( (adcValue / ADC_MAX) * 20 ) + 1;
   
-  if( (lastVolume >= volume && lastVolume-volume > 1) || (volume > lastVolume && volume-lastVolume > 1) ){
-  //if( lastVolume != volume ){
+  //if( (lastVolume >= volume && lastVolume-volume > 1) || (volume > lastVolume && volume-lastVolume > 1) ){
+  if( lastVolume != volume ){
     audio.setVolume( volume );
-    Serial.print("new volume ");
-    Serial.println(volume);
+    //Serial.print("new volume ");
+    //Serial.println(volume);
     lastVolume = volume;    
   }
   
@@ -322,23 +331,23 @@ void playNextFile(){
   uint8_t nRandom = random(0, nFilesLeft-1);
   uint8_t i = 0;
 
-  Serial.print("random = ");
-  Serial.println(nRandom);
+  //Serial.print("random = ");
+  //Serial.println(nRandom);
 
   while(i < 255){
 
     String filename = filesList[i];
     int filenameLength = filename.length();
 
-    Serial.print("> checking ");
-    Serial.println(i);
-    Serial.println(filename);
-    Serial.flush();
+    //Serial.print("> checking ");
+    //Serial.println(i);
+    //Serial.println(filename);
+    //Serial.flush();
 
     // exit on empty string
     if( filenameLength  < 1 ){
       break;
-      Serial.println("> exit, cause of empty string");
+      //Serial.println("> exit, cause of empty string");
     }
 
     // skip used files (min. requried filname length 6, is like "/a.mp3")
@@ -354,10 +363,10 @@ void playNextFile(){
           // open file
           File entry = SD.open( filename, FILE_READ );
           nFilesLeft--;
-          filesList[i] = " ";
+          filesList[i] = "-";   // set filename that is not long enough
       
           // play file
-          Serial.println("> found and play");
+          //Serial.println("> found and play");
           audio.connecttoSD( filename );
       
           entry.close();
